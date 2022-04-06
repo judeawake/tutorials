f4566ccb75f242727635cf8444e8e93266f35bed
unknown <judeawake@gmail.com>
2022-04-06 13:02:18 +0100

    Initial upload

---
+++ Tutorial.js
@@ -0,0 +1,16 @@
+// var T = 20.6787;
+// console.log(T.toFixed(3)+10); +"20.679"+10; // Number("20.679") + 10
+// console.log(T.toExponential(2));
+// console.log(T.toPrecision(4));
+
+var j = "she's is a boy"
+var j = 'she\'s is a boy'
+console.log("jude is a boy\nHe like beans");
+
+var e = "The name of jehovah is a strong tower";
+console.log(e.length);
+console.log(e.indexOf("is"));
+console.log(e.lastIndexOf("n"));
+console.log(e.includes("is"));
+
+// java properties and method//
